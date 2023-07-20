5f433e423acc680f16bc98088f4abc0f89eb7c85
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2023-07-20 15:03:13 -0400

    Add initial passport Google auth

---
+++ server/config/passport.js
@@ -0,0 +1,267 @@
+const passport = require('passport');
+const refresh = require('passport-oauth2-refresh');
+const axios = require('axios');
+const { Strategy: LocalStrategy } = require('passport-local');
+const { Strategy: GitHubStrategy } = require('passport-github2');
+const { OAuth2Strategy: GoogleStrategy } = require('passport-google-oauth');
+const { OAuthStrategy } = require('passport-oauth');
+const { OAuth2Strategy } = require('passport-oauth');
+const _ = require('lodash');
+const moment = require('moment');
+
+const User = require('../models/User');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+        done(err, user);
+    });
+});
+
+/**
+ * Sign in using Email and Password.
+ */
+passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+    User.findOne({ email: email.toLowerCase() }, (err, user) => {
+        if (err) { return done(err); }
+        if (!user) {
+            return done(null, false, { msg: `Email ${email} not found.` });
+        }
+        if (!user.password) {
+            return done(null, false, { msg: 'Your account was registered using a sign-in provider. To enable password login, sign in using a provider, and then set a password under your user profile.' });
+        }
+        user.comparePassword(password, (err, isMatch) => {
+            if (err) { return done(err); }
+            if (isMatch) {
+                return done(null, user);
+            }
+            return done(null, false, { msg: 'Invalid email or password.' });
+        });
+    });
+}));
+
+/**
+ * OAuth Strategy Overview
+ *
+ * - User is already logged in.
+ *   - Check if there is an existing account with a provider id.
+ *     - If there is, return an error message. (Account merging not supported)
+ *     - Else link new OAuth account with currently logged-in user.
+ * - User is not logged in.
+ *   - Check if it's a returning user.
+ *     - If returning user, sign in and we are done.
+ *     - Else check if there is an existing account with user's email.
+ *       - If there is, return an error message.
+ *       - Else create a new account.
+ */
+
+// /**
+//  * Sign in with GitHub.
+//  */
+// passport.use(new GitHubStrategy({
+//     clientID: process.env.GITHUB_ID,
+//     clientSecret: process.env.GITHUB_SECRET,
+//     callbackURL: `${process.env.BASE_URL}/auth/github/callback`,
+//     passReqToCallback: true,
+//     scope: ['user:email']
+// }, (req, accessToken, refreshToken, profile, done) => {
+//     if (req.user) {
+//         User.findOne({ github: profile.id }, (err, existingUser) => {
+//             if (existingUser) {
+//                 req.flash('errors', { msg: 'There is already a GitHub account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
+//                 done(err);
+//             } else {
+//                 User.findById(req.user.id, (err, user) => {
+//                     if (err) { return done(err); }
+//                     user.github = profile.id;
+//                     user.tokens.push({ kind: 'github', accessToken });
+//                     user.profile.name = user.profile.name || profile.displayName;
+//                     user.profile.picture = user.profile.picture || profile._json.avatar_url;
+//                     user.profile.location = user.profile.location || profile._json.location;
+//                     user.profile.website = user.profile.website || profile._json.blog;
+//                     user.save((err) => {
+//                         req.flash('info', { msg: 'GitHub account has been linked.' });
+//                         done(err, user);
+//                     });
+//                 });
+//             }
+//         });
+//     } else {
+//         User.findOne({ github: profile.id }, (err, existingUser) => {
+//             if (err) { return done(err); }
+//             if (existingUser) {
+//                 return done(null, existingUser);
+//             }
+//             if (profile._json.email === null) {
+//                 User.findOne({ email: profile.emails[0].value }, (err, existingEmailUser) => {
+//                     if (err) { return done(err); }
+//                     if (existingEmailUser) {
+//                         req.flash('errors', { msg: 'There is already an account using this email address. Sign in to that account and link it with GitHub manually from Account Settings.' });
+//                         done(err);
+//                     } else {
+//                         const user = new User();
+//                         user.email = _.get(_.orderBy(profile.emails, ['primary', 'verified'], ['desc', 'desc']), [0, 'value'], null);
+//                         user.github = profile.id;
+//                         user.tokens.push({ kind: 'github', accessToken });
+//                         user.profile.name = profile.displayName;
+//                         user.profile.picture = profile._json.avatar_url;
+//                         user.profile.location = profile._json.location;
+//                         user.profile.website = profile._json.blog;
+//                         user.save((err) => {
+//                             done(err, user);
+//                         });
+//                     }
+//                 });
+//             } else {
+//                 User.findOne({ email: profile._json.email }, (err, existingEmailUser) => {
+//                     if (err) { return done(err); }
+//                     if (existingEmailUser) {
+//                         req.flash('errors', { msg: 'There is already an account using this email address. Sign in to that account and link it with GitHub manually from Account Settings.' });
+//                         done(err);
+//                     } else {
+//                         const user = new User();
+//                         user.email = _.get(_.orderBy(profile.emails, ['primary', 'verified'], ['desc', 'desc']), [0, 'value'], null);
+//                         user.github = profile.id;
+//                         user.tokens.push({ kind: 'github', accessToken });
+//                         user.profile.name = profile.displayName;
+//                         user.profile.picture = profile._json.avatar_url;
+//                         user.profile.location = profile._json.location;
+//                         user.profile.website = profile._json.blog;
+//                         user.save((err) => {
+//                             done(err, user);
+//                         });
+//                     }
+//                 });
+//             }
+//         });
+//     }
+// }));
+
+/**
+ * Sign in with Google.
+ */
+const googleStrategyConfig = new GoogleStrategy({
+    clientID: process.env.GOOGLE_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: '/auth/google/callback',
+    passReqToCallback: true
+}, async(req, accessToken, refreshToken, params, profile, done) => {
+    try {
+        if (req.user) {
+            const existingUser = await User.findOne({ google: profile.id });
+
+            if (existingUser && (existingUser.id !== req.user.id)) {
+                req.flash('errors', { msg: 'There is already a Google account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
+                done(null, false);
+            } else {
+                const user = await User.findById(req.user.id);
+                user.google = profile.id;
+                user.tokens.push({
+                    kind: 'google',
+                    accessToken,
+                    accessTokenExpires: moment().add(params.expires_in, 'seconds').format(),
+                    refreshToken,
+                });
+                user.profile.name = user.profile.name || profile.displayName;
+                user.profile.gender = user.profile.gender || profile._json.gender;
+                user.profile.picture = user.profile.picture || profile._json.picture;
+                await user.save();
+                req.flash('info', { msg: 'Google account has been linked.' });
+                done(null, user);
+            }
+        } else {
+            const existingUser = await User.findOne({ google: profile.id });
+            if (existingUser) {
+                done(null, existingUser);
+            } else {
+                const existingEmailUser = await User.findOne({ email: profile.emails[0].value });
+                if (existingEmailUser) {
+                    req.flash('errors', { msg: 'There is already an account using this email address. Sign in to that account and link it with Google manually from Account Settings.' });
+                    done(null, false);
+                } else {
+                    const user = new User();
+                    user.email = profile.emails[0].value;
+                    user.google = profile.id;
+                    user.tokens.push({
+                        kind: 'google',
+                        accessToken,
+                        accessTokenExpires: moment().add(params.expires_in, 'seconds').format(),
+                        refreshToken,
+                    });
+                    user.profile.name = profile.displayName;
+                    user.profile.gender = profile._json.gender;
+                    user.profile.picture = profile._json.picture;
+                    await user.save();
+                    done(null, user);
+                }
+            }
+        }
+    } catch (err) {
+        done(err);
+    }
+});
+
+passport.use('google', googleStrategyConfig);
+refresh.use('google', googleStrategyConfig);
+
+/**
+ * Login Required middleware.
+ */
+exports.isAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/login');
+};
+
+/**
+ * Authorization Required middleware.
+ */
+exports.isAuthorized = (req, res, next) => {
+    const provider = req.path.split('/')[2];
+    const token = req.user.tokens.find((token) => token.kind === provider);
+    if (token) {
+        // Is there an access token expiration and access token expired?
+        // Yes: Is there a refresh token?
+        //     Yes: Does it have expiration and if so is it expired?
+        //       Yes, Quickbooks - We got nothing, redirect to res.redirect(`/auth/${provider}`);
+        //       No, Quickbooks and Google- refresh token and save, and then go to next();
+        //    No:  Treat it like we got nothing, redirect to res.redirect(`/auth/${provider}`);
+        // No: we are good, go to next():
+        if (token.accessTokenExpires && moment(token.accessTokenExpires).isBefore(moment().subtract(1, 'minutes'))) {
+            if (token.refreshToken) {
+                if (token.refreshTokenExpires && moment(token.refreshTokenExpires).isBefore(moment().subtract(1, 'minutes'))) {
+                    res.redirect(`/auth/${provider}`);
+                } else {
+                    refresh.requestNewAccessToken(`${provider}`, token.refreshToken, (err, accessToken, refreshToken, params) => {
+                        User.findById(req.user.id, (err, user) => {
+                            user.tokens.some((tokenObject) => {
+                                if (tokenObject.kind === provider) {
+                                    tokenObject.accessToken = accessToken;
+                                    if (params.expires_in) tokenObject.accessTokenExpires = moment().add(params.expires_in, 'seconds').format();
+                                    return true;
+                                }
+                                return false;
+                            });
+                            req.user = user;
+                            user.markModified('tokens');
+                            user.save((err) => {
+                                if (err) console.log(err);
+                                next();
+                            });
+                        });
+                    });
+                }
+            } else {
+                res.redirect(`/auth/${provider}`);
+            }
+        } else {
+            next();
+        }
+    } else {
+        res.redirect(`/auth/${provider}`);
+    }
+};
--- server/server.js
+++ server/server.js
@@ -1,16 +1,70 @@
-const express = require("express")
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+
+const passport = require("passport");
+
+
+
+// Enable dotenv
+require("dotenv").config({ path: './config/.env' })
+
+/**
+ * API keys and Passport configuration.
+ */
+const passportConfig = require('./config/passport');
+
+
 const app = express()
+
+/**
+ * Connect to MongoDB.
+ */
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on('error', (err) => {
+    console.error(err);
+    console.log('%s MongoDB connection error. Please make sure MongoDB is running.');
+    process.exit();
+});
 
 const PORT = process.env.PORT || 8000;
 const MODE = process.env.NODE_ENV || "development"
 
 // Enable express request url and body parsing
-app.use(express.urlencoded({extended: true}) )
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: process.env.SESSION_SECRET,
+    cookie: { maxAge: 1209600000 }, // Two weeks in milliseconds
+    // store: new MongoStore({ mongoUrl: process.env.MONGODB_URI })
+    store: MongoStore.create({
+        client: mongoose.connection.getClient(),
+    }),
+    cookie: { secure: false }
+}));
+
+// OAuth Routes
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'], accessType: 'offline', prompt: 'consent' }));
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), (req, res) => {
+    res.redirect(req.session.returnTo || '/');
+});
+
+app.get('/auth/github', passport.authenticate('github'));
+app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/login' }), (req, res) => {
+    res.redirect(req.session.returnTo || '/');
+});
 
 // Router(s) config
 app.use('/api', require("./routes/api"))
+app.use('/', (req, res) => {
+  return res.send("WE MADE IT")
+})
 
 app.listen(PORT, () => {
-  console.log(`Starting server on port ${PORT} in ${MODE} mode`);
+    console.log(`Starting server on port ${PORT} in ${MODE} mode`);
 })
