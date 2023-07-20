def686e01500c6b9daa5f795967db30d09733728
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2023-07-20 16:44:22 -0400

    Setup initial React components

---
+++ client/src/components/StickyNote.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function StickyNote() {
+  return (
+    <div>StickyNote</div>
+  )
+}
+
+export default StickyNote
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+function HomePage() {
+  return (
+    <>
+      <Header />
+      <div>HomePage</div>
+      <Footer />
+    </>
+  )
+}
+
+export default HomePage
