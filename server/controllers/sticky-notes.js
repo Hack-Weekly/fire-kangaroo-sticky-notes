d89f5e1cbf4bfe7e1060202aef7fefb228e362d4
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2023-07-21 12:30:36 -0400

    Create API and auth controllers and apply ensureAuth to API routes

---
+++ server/controllers/sticky-notes.js
@@ -0,0 +1,38 @@
+const User = require("../models/User");
+const StickyNote = require("../models/StickyNote");
+
+module.exports = {
+    notes: async (req, res) => {
+        let notes = await StickyNote
+            .find({ user: req.session.passport.user })
+            .select(["title", "text"])
+        console.log(notes)
+        res.json(notes)
+    },
+
+    add: async (req, res) => {
+        let user_id = req.body.user_id;
+
+        try {
+            let user = await User.findById(user_id);
+            let note = await StickyNote.create({
+                user: user,
+                title: req.body?.title,
+                text: req.body?.text,
+            })
+            res.json({ "success": true })
+        } catch (err) {
+            console.log(`User: ${user_id} does not exist`, err)
+            res.json({ "success": false })
+        }
+    },
+    delete: async (req, res) => {
+        try {
+            await StickyNote.findOneAndDelete({ _id: req.body._id })
+            res.json({ "success": true })
+        } catch (err) {
+            console.log(err)
+            res.json({ "success": false })
+        }
+    }
+}
--- server/routes/api.js
+++ server/routes/api.js
@@ -4,35 +4,11 @@ const router = express.Router();
 const User = require("../models/User");
 const StickyNote = require("../models/StickyNote");
 
-router.get("/note/:id", (req, res) => {
-    return res.send(req.params.id)
-})
+const authController = require("../controllers/auth");
+const stickyNotesController = require("../controllers/sticky-notes");
 
-router.post("/add", async(req, res) => {
-    let user_id = req.body.user_id;
-
-    try {
-        let user = await User.findById(user_id);
-        let note = await StickyNote.create({
-            user: user,
-            title: req.body?.title,
-            text: req.body?.text,
-        })
-        res.json({ "success": true })
-    } catch (err) {
-        console.log(`User: ${user_id} does not exist`, err)
-        res.json({ "success": false })
-    }
-})
-
-router.post("/delete", async (req, res) => {
-    try {
-        await StickyNote.findOneAndDelete( {_id: req.body._id} )
-        res.json({ "success": true })
-    } catch (err) {
-        console.log(err)
-        res.json({ "success": false })
-    }
-})
+router.get("/notes", authController.ensureAuth, stickyNotesController.notes);
+router.post("/add", authController.ensureAuth, stickyNotesController.add);
+router.post("/delete", authController.ensureAuth, stickyNotesController.delete);
 
 module.exports = router
