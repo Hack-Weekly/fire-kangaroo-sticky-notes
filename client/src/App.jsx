5ce7f0333f3142c799956d902e64826fa73a2f6f
AhmedHHamdy <ahmedhhamdyad@gmail.com>
2023-07-21 19:09:28 +0300

    setting up edit note  page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 
 import HomePage from "./pages/HomePage";
+import StickyNoteEdit from "./pages/StickyNoteEdit"
 
 function App() {
     return (
         <div className="App" >
-            <HomePage />
+            <StickyNoteEdit />
+            {/* <HomePage /> */}
         </div>
     );
 }
--- client/src/pages/StickyNoteEdit.jsx
+++ client/src/pages/StickyNoteEdit.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Header from '../components/Header'
+import '../App.css'
 
 function StickyNoteEdit() {
   return (
-    <div>StickyNoteEdit</div>
+    <>
+      <Header />
+      <section className='edit-section'>
+        <div className='nav-buttons'></div>
+          <section>
+            <form action="">
+              <input type="text" name='title' placeholder='Title'/>
+              <textarea name='title' placeholder='Type something...'/>
+              <button className='save-button'>Save</button>
+            </form>
+            <button className='cancel-button'>Cancel</button>
+          </section>
+      </section>
+    </>
+
   )
 }
 
