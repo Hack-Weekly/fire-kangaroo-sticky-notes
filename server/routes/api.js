48b02a867b0771b761a6f81635f7331de3300f25
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2023-07-21 11:31:23 -0400

    Add StickyNote model and add/delete API routes

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,38 @@
 const express = require("express");
-const router = express.Router()
+const router = express.Router();
 
-router.get("/", (req, res) => {
-    return res.send({ "api": "test" })
+const User = require("../models/User");
+const StickyNote = require("../models/StickyNote");
+
+router.get("/note/:id", (req, res) => {
+    return res.send(req.params.id)
+})
+
+router.post("/add", async(req, res) => {
+    let user_id = req.body.user_id;
+
+    try {
+        let user = await User.findById(user_id);
+        let note = await StickyNote.create({
+            user: user,
+            title: req.body?.title,
+            text: req.body?.text,
+        })
+        res.json({ "success": true })
+    } catch (err) {
+        console.log(`User: ${user_id} does not exist`, err)
+        res.json({ "success": false })
+    }
+})
+
+router.post("/delete", async (req, res) => {
+    try {
+        await StickyNote.findOneAndDelete( {_id: req.body._id} )
+        res.json({ "success": true })
+    } catch (err) {
+        console.log(err)
+        res.json({ "success": false })
+    }
 })
 
 module.exports = router
-
