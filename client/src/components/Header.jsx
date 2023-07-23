88275df209465fef15fda113d5c81fac09a7c8be
Jose Ruiz <joeruizl87@gmail.com>
2023-07-23 15:02:07 -0400

    Add

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   const [open, setOpen] = useState(false)
   return (
     <div style={{ display: 'flex', justifyContent: 'space-between', fontWeight: '300px', padding: "20px 0px 20px 0px ", backgroundColor: "var(--bg-300)" }}>
-      <div style={{ marginLeft: "40px", padding: "10px", color: "var(--text-200)", fontSize: "1.3rem" }}>Sticky Notes</div>
+      <Link style={{ marginLeft: "40px", padding: "10px", color: "var(--text-200)", fontSize: "1.3rem" }} to="/">Sticky Notes</Link>
       <div>
         <PersonFill onClick={() => setOpen(!open)} style={{ position: "relative", marginRight: "50px", fontSize: "2.5rem", backgroundColor: "var(--bg-300)", borderRadius: "50%", backgroundColor: "var(--text-200)" }}></PersonFill>
         {
