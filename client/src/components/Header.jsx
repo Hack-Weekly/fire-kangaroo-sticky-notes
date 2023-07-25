44a6856fe69aa1e82dde1128ad0d672e41c68b22
Jose Ruiz <joeruizl87@gmail.com>
2023-07-24 20:11:30 -0400

    updating header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,22 +8,26 @@ function Header() {
     <div style={{ display: 'flex', justifyContent: 'space-between', fontWeight: '300px', padding: "20px 0px 20px 0px ", backgroundColor: "var(--bg-300)" }}>
       <Link style={{ marginLeft: "40px", padding: "10px", color: "var(--text-200)", fontSize: "1.3rem", textDecoration: "none" }} to="/">Sticky Notes</Link>
       <div>
-        <PersonFill onClick={() => setOpen(!open)} style={{ position: "relative", marginRight: "50px", fontSize: "2.5rem", backgroundColor: "var(--bg-300)", borderRadius: "50%", backgroundColor: "var(--text-200)" }}></PersonFill>
+        <button style={{ position: "relative", marginRight: "75px", fontSize: "2.05rem", backgroundColor: "var(--bg-300)", borderRadius: "50%", backgroundColor: "var(--text-200)" }}><PersonFill style={{ paddingBottom: "6px" }} onClick={() => setOpen(!open)} ></PersonFill></button>
         {
           open && (
-            <div style={{ position: "absolute", right: "45px" }} > {
-              <ul style={{ listStyle: "none", fontSize: "3rem", fontWeight: "bolder" }}>
-                <li>
-                  <Link style={{ textDecoration: "none", color: "var(--text-200)", fontSize: "0.8rem", }} to="/login">Login</Link>
-                </li>
-                <li>
-                  <Link style={{ textDecoration: "none", color: "var(--text-200)", fontSize: "0.8rem" }} to="/signup">Signup</Link>
-                </li>
-                <li>
-                  <Link style={{ textDecoration: "none", color: "var(--text-200)", fontSize: "0.8rem" }} to="/logout">Logout</Link>
-                </li>
-              </ul>
-            }</div>
+            <div>
+              <div id="dropdown" ></div>
+              <div style={{ position: "absolute", right: "45px", borderRadius: "15px", backgroundColor: "var(--text-200)", width: "110px" }} > {
+                <ul style={{ listStyle: "none", fontSize: "2rem", fontWeight: "normal", fontFamily: "sans-serif", justifyItems: "center" }}>
+                  <li>
+                    <Link style={{ textDecoration: "none", color: "var(--bg-300)", fontSize: "1rem" }} to="/login">Login</Link>
+                  </li>
+                  <li>
+                    <Link style={{ textDecoration: "none", color: "var(--bg-300)", fontSize: "1rem" }} to="/signup">Signup</Link>
+                  </li>
+                  <li>
+                    <Link style={{ textDecoration: "none", color: "var(--bg-300)", fontSize: "1rem" }} to="/logout">Logout</Link>
+                  </li>
+                </ul>
+              }</div>
+            </div>
+
           )
         }
 
